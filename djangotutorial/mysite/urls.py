eec8fac59c86630768773af51a41126475e086ca
Danielle Phillips <69817991+Peanut16@users.noreply.github.com>
2025-10-28 15:01:00 -0400

    Change virtual environment to python3.13.9 to get the right version of django

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from django.contrib import admin, settings
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
-from django.conf import settings
+
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
