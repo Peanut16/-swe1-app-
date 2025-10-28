b70b2078be51f9cebe2e5d16c755332f9d1b2547
Danielle Phillips <69817991+Peanut16@users.noreply.github.com>
2025-10-28 15:30:14 -0400

    attempted to add coveralls

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Coverage Status](https://coveralls.io/repos/github/Peanut16/-swe1-app-/badge.svg?branch=main)](https://coveralls.io/github/Peanut16/-swe1-app-?branch=main)
--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin, settings
+from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 
